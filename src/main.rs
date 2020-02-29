35708fdc1c600c04ce1dc03a0c9ea36e9705162c
Albrecht <14820950+weichweich@users.noreply.github.com>
2020-02-29 22:25:13 +0100

    feat: floodsub, topic, better errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use clap::{App, Arg, ArgMatches};
 use libp2p::{
-    identity::{Keypair, ed25519},
-    tcp::TcpConfig, PeerId, Multiaddr, Transport,
+    floodsub::{self, Floodsub, FloodsubEvent},
+    identity::{ed25519, Keypair},
+    swarm::NetworkBehaviourEventProcess,
+    NetworkBehaviour, PeerId, Swarm,
 };
 use log;
+use std::{
+    error::Error,
+    fs::File,
+    io::{Read, Write},
+};
 use stderrlog;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::error::Error;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHOR: &str = env!("CARGO_PKG_AUTHORS");
@@ -16,6 +20,9 @@ const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 const ARG_VERBOSE: &str = "verbose";
 const ARG_KEYPAIR: &str = "keypair";
 const ARG_GEN: &str = "keygen";
+const ARG_TOPIC: &str = "topic";
+
+const DEFAULT_TOPIC: &str = "chat";
 
 fn get_keypair(matches: &ArgMatches) -> Result<Keypair, Box<dyn Error>> {
     if matches.occurrences_of(ARG_GEN) > 0 {
@@ -36,7 +43,9 @@ fn get_keypair(matches: &ArgMatches) -> Result<Keypair, Box<dyn Error>> {
         let mut file = File::open(filename)?;
         file.read_to_end(&mut keydata)?;
 
-        Ok(Keypair::Ed25519(ed25519::Keypair::decode(&mut keydata[..])?))
+        Ok(Keypair::Ed25519(ed25519::Keypair::decode(
+            &mut keydata[..],
+        )?))
     } else {
         let err_msg = format!(
             "Argument <{}> is required if <{}> is not set.",
@@ -48,8 +57,62 @@ fn get_keypair(matches: &ArgMatches) -> Result<Keypair, Box<dyn Error>> {
     }
 }
 
-fn main() {
-    let matches = App::new("Püür tö püür Fün")
+#[derive(NetworkBehaviour)]
+struct Chat {
+    floodsub: Floodsub,
+
+    // Struct fields which do not implement NetworkBehaviour need to be ignored
+    #[behaviour(ignore)]
+    #[allow(dead_code)]
+    ignored_member: bool,
+}
+
+impl NetworkBehaviourEventProcess<FloodsubEvent> for Chat {
+    // Called when `floodsub` produces an event.
+    fn inject_event(&mut self, message: FloodsubEvent) {
+        if let FloodsubEvent::Message(message) = message {
+            println!(
+                "Received: '{:?}' from {:?}",
+                String::from_utf8_lossy(&message.data),
+                message.source
+            );
+        }
+    }
+}
+
+fn execute_app(matches: ArgMatches) -> Result<(), Box<dyn Error>> {
+    // Generate new key pair
+    let keypair = get_keypair(&matches)?;
+
+    // Create PeerID
+    let local_peer_id = PeerId::from(keypair.public());
+
+    // Create transport protocol
+    // TODO: use production transport
+    let transport = libp2p::build_development_transport(keypair)?;
+
+    let mut behaviour = Chat {
+        floodsub: Floodsub::new(local_peer_id.clone()),
+        ignored_member: false,
+    };
+
+    // Create a  floodsub topic
+    let topic = matches.value_of(ARG_TOPIC).ok_or("Topic missing?")?;
+    let floodsub_topic = floodsub::Topic::new(topic);
+    log::info!("Topic is: {}", &topic);
+
+    // Create a Swarm to manage peers and events
+    behaviour.floodsub.subscribe(floodsub_topic);
+    let mut _swarm = Swarm::new(transport, behaviour, local_peer_id);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let gen_arg_help = format!(
+        "Generate a new key pair and write it to <{}>.",
+        ARG_KEYPAIR
+    );
+    let app = App::new("Püür tö püür Fün")
         .version(VERSION)
         .author(AUTHOR)
         .about(DESCRIPTION)
@@ -70,27 +133,26 @@ fn main() {
             Arg::with_name(ARG_GEN)
                 .short("g")
                 .takes_value(false)
-                .help(&format!(
-                    "Generate a new key pair and write it to <{}>.",
-                    ARG_KEYPAIR
-                )),
+                .help(&gen_arg_help),
         )
-        .get_matches();
+        .arg(
+            Arg::with_name(ARG_TOPIC)
+                .short("t")
+                .help("The topic to chat on.")
+                .takes_value(true)
+                .default_value(DEFAULT_TOPIC),
+        );
+    let matches = app.get_matches();
 
     // Configure logger
     stderrlog::new()
         .verbosity(matches.occurrences_of(ARG_VERBOSE) as usize)
-        .init()
-        .unwrap();
+        .init()?;
 
-    // Generate new key pair
-    let keypair = get_keypair(&matches).unwrap();
-
-    // Create PeerID
-    let local_peer_id = PeerId::from(keypair.public());
-
-    // Create transport protocol
-    let transport = TcpConfig::new();
-    let addr: Multiaddr = "/ip6/98.97.96.95/tcp/0".parse().expect("invalid multiaddr");
-    let _conn = transport.dial(addr);
+    if let Err(box_err) = execute_app(matches) {
+        eprintln!("Application error:\n{}", box_err);
+        Err(box_err)
+    } else {
+        Ok(())
+    }
 }
